c66b235ef5e8507abc58177ab5fc0add6378e64b
Byron Alley <byron@byronalley.com>
2025-08-28 14:57:17 -0700

    Handle comma and PDT vs PST or PT

--- src/main.rs
+++ src/main.rs
@@ -21,42 +21,9 @@ fn main() {
     }
 }
 
-#[test]
-fn test_convert_time() {
-    let input_times = vec![
-        "Wed Jun 11, 9:00am-10:30am PST",
-        "Wed Jun 11, 1:00pm-2:30pm PST",
-        "Thu Jun 12, 10:00am-11:30am PST",
-        "Fri Jun 13, 9:30am-11:00am PST",
-        "Mon Jun 16, 1:00pm-2:30pm PST",
-        "Tue Jun 17, 10:00am-11:30am PST",
-    ];
-
-    let expected_patterns = vec![
-        Regex::new(r"Wed Jun 11, 9:00am-10:30am PST \(12:00pm-1:30pm E[DS]T\)").unwrap(),
-        Regex::new(r"Wed Jun 11, 1:00pm-2:30pm PST \(4:00pm-5:30pm E[DS]T\)").unwrap(),
-        Regex::new(r"Thu Jun 12, 10:00am-11:30am PST \(1:00pm-2:30pm E[DS]T\)").unwrap(),
-        Regex::new(r"Fri Jun 13, 9:30am-11:00am PST \(12:30pm-2:00pm E[DS]T\)").unwrap(),
-        Regex::new(r"Mon Jun 16, 1:00pm-2:30pm PST \(4:00pm-5:30pm E[DS]T\)").unwrap(),
-        Regex::new(r"Tue Jun 17, 10:00am-11:30am PST \(1:00pm-2:30pm E[DS]T\)").unwrap(),
-    ];
-
-    let tz: Tz = "America/New_York".parse().unwrap();
-
-    for (input, expected) in input_times.iter().zip(expected_patterns.iter()) {
-        let result = convert_time(input, tz).unwrap();
-        assert!(
-            expected.is_match(&result),
-            "Expected {} to match pattern {}",
-            result,
-            expected.as_str()
-        );
-    }
-}
-
 fn convert_time(time_str: &str, target_tz: Tz) -> Result<String, String> {
     let re = RegexBuilder::new(
-        r"(\w+\s+\w+\s+\d+),\s*(\d+:\d{2}(?:am|pm))-(\d+:\d{2}(?:am|pm))\s+P[SD]T",
+        r"(\w+\s+\w+\s+\d+),?\s*(\d+:\d{2}(?:am|pm))-(\d+:\d{2}(?:am|pm))\s+[A-Z]{1,2}T",
     )
     .case_insensitive(true)
     .build()
@@ -125,4 +92,50 @@ fn parse_datetime(year: i32, month: &str, day: &str, time: &str) -> Result<DateT
         .from_local_datetime(&naive_dt)
         .single()
         .ok_or("Ambiguous or invalid datetime")?)
+}
+
+#[cfg(test)]
+mod test {
+    use super::convert_time;
+    use chrono_tz::Tz;
+    use regex::Regex;
+    #[test]
+    fn test_convert_time() {
+        let test_data = vec![
+            (
+                "Wed Jun 11, 5:00am-8:30am PDT",
+                r"Wed Jun 11, 5:00am-8:30am PDT \(8:00am-11:30am E[DS]T\)",
+            ),
+            (
+                "Wed Jun 11, 9:00am-10:30am PDT",
+                r"Wed Jun 11, 9:00am-10:30am PDT \(12:00pm-1:30pm E[DS]T\)",
+            ),
+            (
+                "Wed Dec 11, 10:00am-2:30pm PST",
+                r"Wed Dec 11, 10:00am-2:30pm PST \(1:00pm-5:30pm E[DS]T\)",
+            ),
+            (
+                "Mon Jun 16, 1:00pm-2:30pm PT",
+                r"Mon Jun 16, 1:00pm-2:30pm PT \(4:00pm-5:30pm E[DS]T\)",
+            ),
+            (
+                "Tue Sep 17 10:00am-11:30am PST",
+                r"Tue Sep 17 10:00am-11:30am PST \(1:00pm-2:30pm E[DS]T\)",
+            ),
+        ];
+
+        let tz: Tz = "America/New_York".parse().unwrap();
+
+        for (input, expected_str) in test_data.iter() {
+            let expected = Regex::new(expected_str).unwrap();
+
+            let result = convert_time(input, tz).unwrap();
+            assert!(
+                expected.is_match(&result),
+                "Expected {} to match pattern {}",
+                result,
+                expected.as_str()
+            );
+        }
+    }
 }
